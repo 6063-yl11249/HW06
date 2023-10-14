497995714cf7d1f6c24a73ca10dfcbf5914512e9
Yuanqing <yl11249@nyu.edu>
2023-10-14 18:08:32 -0400

    class

--- sketch.js
+++ sketch.js
@@ -1,36 +1,56 @@
-let mFont;
-let mSize = 120;
+class WavyText {
+  constructor(text, font, size, color) {
+    this.text = text;
+    this.font = font;
+    this.size = size;
+    this.color = color;
+    this.wordPoints = [];
+  }
 
-let word = "Halloween Party";
-let wordPoints;
+  setup() {
+    this.wordPoints = this.font.textToPoints(this.text, 0, 0, this.size, {
+      sampleFactor: 1,
+    });
+    noFill();
+    stroke(this.color);
+  }
 
-function preload() {
-  mFont = loadFont("./Excalibur Nouveau.ttf");
+  draw() {
+    background('orange');
+    let randMax = map(mouseX, 0, width, 0, 16);
+    translate((width - textWidth(this.text)) / 2, (height + this.size) / 2);
+
+    beginShape();
+    for (let i = 0; i < this.wordPoints.length; i++) {
+      let p = this.wordPoints[i];
+      let rx = random(-randMax, randMax);
+      let ry = random(-randMax, randMax);
+      vertex(p.x + rx, p.y + ry);
+    }
+    endShape();
+  }
 }
 
+// Usage
+let tFont;
+let bFont;
+
+function preload() {
+  tFont = loadFont("./Excalibur Nouveau.ttf");
+  bFont = loadFont("./Alstoria Demo.ttf");
+}
+
+let wavyText;
+
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  textFont(mFont);
-  textSize(mSize);
-  wordPoints = mFont.textToPoints(word, 0, 0, mSize, {
-    sampleFactor: 1,
-  });
-  noFill();
-  stroke('purple');
+  createCanvas(windowWidth, windowHeight);
+  textSize(120);
+  wavyText = new WavyText("Halloween Party", tFont, 120, 'purple');
+  wavyText.setup();
+
+
 }
 
 function draw() {
-  background('orange');
-
-  let randMax = map(mouseX, 0, width, 0, 16);
-  translate((width - textWidth(word)) / 2, (height + mSize) / 2);
-
-  beginShape();
-  for (let i = 0; i < wordPoints.length; i++) {
-    let p = wordPoints[i];
-    let rx = random(-randMax, randMax);
-    let ry = random(-randMax, randMax);
-    vertex(p.x + rx, p.y + ry);
-  }
-  endShape();
+  wavyText.draw();
 }
