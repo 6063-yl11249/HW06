5df0d9ae32f9be4508ff222663a819a9d877bf6a
Yuanqing <yl11249@nyu.edu>
2023-10-14 17:55:21 -0400

    Halloween Party

--- sketch.js
+++ sketch.js
@@ -1,6 +1,36 @@
+let mFont;
+let mSize = 120;
+
+let word = "Halloween Party";
+let wordPoints;
+
+function preload() {
+  mFont = loadFont("./Excalibur Nouveau.ttf");
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  textFont(mFont);
+  textSize(mSize);
+  wordPoints = mFont.textToPoints(word, 0, 0, mSize, {
+    sampleFactor: 1,
+  });
+  noFill();
+  stroke('purple');
 }
 
 function draw() {
+  background('orange');
+
+  let randMax = map(mouseX, 0, width, 0, 16);
+  translate((width - textWidth(word)) / 2, (height + mSize) / 2);
+
+  beginShape();
+  for (let i = 0; i < wordPoints.length; i++) {
+    let p = wordPoints[i];
+    let rx = random(-randMax, randMax);
+    let ry = random(-randMax, randMax);
+    vertex(p.x + rx, p.y + ry);
+  }
+  endShape();
 }
