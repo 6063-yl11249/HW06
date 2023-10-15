877b822f1b102683ea557cef9809603b0239dba2
Yuanqing <yl11249@nyu.edu>
2023-10-15 16:28:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ let phrase;
 let bFont;
 let selectedWords = [];
 
-
 class WavyText {
   constructor(text, font, size, color, x, y) {
     this.text = text;
@@ -10,14 +9,20 @@ class WavyText {
     this.size = size;
     this.color = color;
     this.x = x;
-    this.y = y + textAscent(); 
+    this.y = y + textAscent();
     this.wordPoints = [];
   }
 
   setup() {
-    this.wordPoints = this.font.textToPoints(this.text, this.x, this.y, this.size, {
-      sampleFactor: 1,
-    });
+    this.wordPoints = this.font.textToPoints(
+      this.text,
+      this.x,
+      this.y,
+      this.size,
+      {
+        sampleFactor: 1,
+      }
+    );
     noFill();
     stroke(this.color);
   }
@@ -45,47 +50,43 @@ function preload() {
   wFont = loadFont("./Excalibur Nouveau.ttf");
 }
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let words = join(phrase, " ").split(" ");
-
- 
 
   for (let i = 0; i < 9; i++) {
     let index = floor(random(words.length));
     selectedWords.push(words[index]);
   }
-
 }
 
 function draw() {
-  background('red');
+  background("red");
   textFont(bFont);
   textSize(68);
   textAlign(LEFT, TOP);
 
   let x = 80;
   let y = 40;
-  let space = textWidth(' ');
+  let space = textWidth(" ");
 
   let words = join(phrase, " ").split(" ");
 
   for (let word of words) {
     if (selectedWords.includes(word)) {
-      let wavyWord = new WavyText(word, wFont, 72, 'black', x, y);
+      let wavyWord = new WavyText(word, wFont, 48, "black", x, y);
       wavyWord.setup();
       wavyWord.draw();
     } else {
-      fill(255);      
-      noStroke();    
+      fill(255);
+      noStroke();
       textFont(bFont);
-      textSize (64);
+      textSize(38);
       text(word, x, y);
     }
 
     x += textWidth(word) + space;
-    if (x > width - 300) {
+    if (x > width - 200) {
       x = 80;
       y += 90;
     }
